a131e299725aa8da6ee50050f599d0c9d93029f6
SaraCremonesi <saracrem00@gmail.com>
2020-07-14 15:58:35 +0200

    esercizio primi step php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+  $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+  $badWord = $_GET['badword'];
+
+  $testoCensurato = str_replace($badWord, '***', $testo);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>La lunghezza del testo originale è di caratteri: <?php echo strlen($testo); ?></h3>
+    <h4>Le "bad words" nel testo seguente verranno rimpiazzate da tre asterischi</h4>
+    <p>
+      <?php
+        echo $testoCensurato;
+      ?>
+    </p>
+  </body>
+</html>
